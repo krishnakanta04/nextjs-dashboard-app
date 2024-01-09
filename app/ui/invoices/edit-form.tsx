6835110155000808bc69e734853bf2f696a657a5
Krishnakanta Naik <krishnakantanaik04@gmail.com>
2024-01-09 20:56:55 +0530

    Improving accessibility

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -11,6 +11,7 @@ import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
 import { updateInvoice } from '@/app/lib/action';
+import { useFormState } from 'react-dom';
 
 export default function EditInvoiceForm({
   invoice,
@@ -19,10 +20,13 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
 
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
+  console.log(state);
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -47,6 +51,11 @@ export default function EditInvoiceForm({
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+          {state.errors?.customerId && (
+            <p className="mt-2 text-sm text-red-500">
+              {state.errors.customerId[0]}
+            </p>
+          )}
         </div>
 
         {/* Invoice Amount */}
@@ -68,6 +77,11 @@ export default function EditInvoiceForm({
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
+          {state.errors?.amount && (
+            <p className="mt-2 text-sm text-red-500">
+              {state.errors.amount[0]}
+            </p>
+          )}
         </div>
 
         {/* Invoice Status */}
@@ -111,6 +125,11 @@ export default function EditInvoiceForm({
               </div>
             </div>
           </div>
+          {state.errors?.status && (
+            <p className="mt-2 text-sm text-red-500">
+              {state.errors.status[0]}
+            </p>
+          )}
         </fieldset>
       </div>
       <div className="mt-6 flex justify-end gap-4">
