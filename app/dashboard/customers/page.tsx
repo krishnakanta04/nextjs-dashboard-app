3afcae3594ee6aea00aeb808c329fdb4d3bdecbc
Krishnakanta Naik <krishnakantanaik04@gmail.com>
2024-01-10 11:29:53 +0530

    Added Metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
-export default function Customers(){
-    return <p>Customers Page</p>
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default function Customers() {
+  return <p>Customers Page</p>;
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,14 +6,23 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
- 
-export default async function Page({searchParams}: {searchParams ?: {query?: string, page?: string}}) {
-  const query = searchParams?.query || ''
-  const currentPage = Number(searchParams?.page) || 1
+import { Metadata } from 'next';
 
-  const totalPages = await fetchInvoicesPages(query)
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
-    return (
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
+
+  return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
@@ -22,7 +31,7 @@ export default async function Page({searchParams}: {searchParams ?: {query?: str
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
